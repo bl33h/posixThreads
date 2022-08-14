0181946846a9d4bd4aa2aaa272ae754231b1dfdf
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-08-13 22:25:26 -0600

    main method placed.

--- PosixThreads.cpp
+++ PosixThreads.cpp
@@ -4,5 +4,22 @@ Copyright (C), 2022-2023, Sara Echeverria (bl33h)
 FileName: PosixThreads
 @version: I
 Creation: 11/08/2022
-Last modification: 11/08/2022
+Last modification: 13/08/2022
 ------------------------------------------------------------------------------*/
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <unistd.h>
+
+// Main method for both threads
+int main{
+    pthread_t tid, tid1;
+    t = pthread_create(&tid, NULL, sub, (void *)message);
+    t1 = pthread_create(&tid1, NULL, sub1, (void *)message1);
+    pthread_join( tid, NULL);
+    pthread_join( tid1, NULL);
+    exit(0);
+    }
